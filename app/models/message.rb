7ecad8be5c3eed412a7410f5c87c7e511725bcff
yuichiro_suzuki <ninokashu@docomo.ne.jp>
2016-03-12 06:50:49 +0000

    challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,7 @@ class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20 } , presence: true
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
+  # 値段は数値で0以上
+  validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
 
